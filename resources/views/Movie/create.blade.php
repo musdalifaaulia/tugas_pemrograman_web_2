57990d6ff15adff3f6379517ab4b5e602d17ce3d
Musdalifa Aulia Pratiwi <musdalifahaulia4@gmail.com>
2026-05-31 14:24:48 +0800

    feat: penyiapan blade component untuk layout utama

---
+++ resources/views/Movie/create.blade.php
@@ -0,0 +1,6 @@
+<x-app>
+
+    <x-slot:title>{{ $title }}</x-slot>
+
+    <h1 class="fw-bold">Create Movie</h1>
+</x-app>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/Movie', function () {
+    return view('Movie.index', ['title' => 'Movie']);
+});
+
+Route::get('/Movie/create', function () {
+    return view('Movie.create', ['title' => 'Create Movie']);
+});
