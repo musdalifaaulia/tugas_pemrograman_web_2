ba76b6c994a061aaf8d5f0bc32e45916e7ee0956
Musdalifa Aulia Pratiwi <musdalifahaulia4@gmail.com>
2026-06-01 15:30:55 +0800

    feat: implementasi form tambah data Review yang terintegrasi dengan pilihan Genre

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -39,7 +39,10 @@ class ReviewController extends Controller
      */
     public function create()
     {
-        //
+        return view('review.create', 
+        ['title' => 'Tambah Review',
+        'genres' => Genre::latest()->get(),
+        ]);
     }
 
     /**
@@ -47,7 +50,43 @@ class ReviewController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+
+    'nama_pengguna' => 'required|max:255',
+
+    'komentar' => 'required|max:255',
+
+    'rating' => 'required|integer|min:1|max:10',
+
+    'tanggal_review' => 'required|date',
+
+    'genre_id' => 'required|exists:genres,id',
+
+], [
+
+    'nama_pengguna.required' => 'Nama Pengguna tidak boleh kosong',
+    'nama_pengguna.max' => 'Nama Pengguna maksimal 255 karakter',
+
+    'komentar.required' => 'Komentar tidak boleh kosong',
+    'komentar.max' => 'Komentar maksimal 255 karakter',
+
+    'rating.required' => 'Rating tidak boleh kosong',
+    'rating.integer' => 'Rating harus berupa angka',
+    'rating.min' => 'Rating minimal 1',
+    'rating.max' => 'Rating maksimal 10',
+
+    'tanggal_review.required' => 'Tanggal Review tidak boleh kosong',
+    'tanggal_review.date' => 'Format Tanggal Review tidak valid',
+
+    'genre_id.required' => 'Genre tidak boleh kosong',
+    'genre_id.exists' => 'Genre yang dipilih tidak ditemukan',
+
+]);
+
+Review::create($validated);
+
+return to_route('review.index')
+    ->withSuccess('Data Review berhasil ditambahkan');
     }
 
     /**
